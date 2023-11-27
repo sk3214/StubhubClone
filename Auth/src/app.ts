c215f242d5bf21e26875ba6904f007697307b86b
unknown <sagarkhuteta26@gmail.com>
2023-11-27 17:40:57 -0500

    Ingress service

--- Auth/src/app.ts
+++ Auth/src/app.ts
@@ -1,9 +1,14 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/api/users/currentuser', (req, res) => {
+    console.log('This is the current user endpoint');
+    res.send('Hi there!');
+});
+
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log('Server is running on port 3000!!');
 });
