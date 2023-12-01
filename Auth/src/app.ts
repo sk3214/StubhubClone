633eb58d2fb5d5cafbf70c3a86c3f0058ce8af93
unknown <sagarkhuteta26@gmail.com>
2023-11-30 22:02:09 -0500

    Defining routes

--- Auth/src/app.ts
+++ Auth/src/app.ts
@@ -1,9 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { currentUserRouter } from './routes/currentUser';
+import { signInRouter } from './routes/signIn';
+import { signOutRouter } from './routes/signOut';
+import { signUpRouter } from './routes/signUp';
 
 const app = express();
 app.use(bodyParser.json());
 
+// app.get('/api/users/currentuser', (req, res) => {
+//     console.log('This is the current user route');
+//     res.send('Hi there!');
+// });
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!!');
+    console.log('Server is running on port 3000!!!!!!!!1');
 });
--- Auth/src/routes/currentUser.ts
+++ Auth/src/routes/currentUser.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/api/users/currentuser', (req, res) => {
+router.get('/api/users/currentuser', (req, res) => {
 });
 
 export { router as currentUserRouter };
