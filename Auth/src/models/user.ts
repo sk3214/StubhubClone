31a043c2a907a21e90ea956bdb7968cb0b5821e8
unknown <sagarkhuteta26@gmail.com>
2023-12-01 19:30:29 -0500

    Added mongoose

---
+++ Auth/src/models/user.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+// An interface that describes the properties that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+    // createdAt: string;
+    // updatedAt: string;
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs);
+}
+
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+
+export { User };
