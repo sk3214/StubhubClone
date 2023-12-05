7c85296eba02d997efa01eb369822cc06d6955dc
unknown <sagarkhuteta26@gmail.com>
2023-12-05 12:23:31 -0500

    Added Secret

--- Auth/src/app.ts
+++ Auth/src/app.ts
@@ -1,14 +1,22 @@
 import express from 'express';
+import 'express-async-errors';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 import { currentUserRouter } from './routes/currentUser';
 import { signInRouter } from './routes/signIn';
 import { signOutRouter } from './routes/signOut';
 import { signUpRouter } from './routes/signUp';
 import { errorHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(bodyParser.json());
-
+app.set('trust proxy', true);
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}));
 // app.get('/api/users/currentuser', (req, res) => {
 //     console.log('This is the current user route');
 //     res.send('Hi there!');
@@ -17,8 +25,25 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+app.all('*', async (req, res, next) => {
+    throw new NotFoundError();
+}
+);
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!!!!!!!!1');
-});
+const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error(err);
+    }
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000!!!');
+    });
+}
+
+start();
--- Auth/src/routes/signUp.ts
+++ Auth/src/routes/signUp.ts
@@ -1,21 +1,42 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
+import jwt from 'jsonwebtoken';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         throw new RequestValidationError(errors.array());
     }
     const { email, password } = req.body;
-    throw new DatabaseConnectionError();
-    res.send({});
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        res.send({});
+    }
+    const user = User.build({ email, password });
+    try {
+        await user.save();
+    } catch (err) {
+        throw new BadRequestError('Email in Use!');
+    }
+    // Generate JWT and store it on the session object
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!);
+    req.session = {
+        jwt: userJwt
+    };
+    res.status(201).send(user);
 });
 
 export { router as signUpRouter };
