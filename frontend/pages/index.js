22f412f085ba43c355cb4d09a20b2223586b1de7
unknown <sagarkhuteta26@gmail.com>
2023-12-11 15:19:17 -0500

    Cleaning up frontend

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -8,29 +8,10 @@ const HomePage = ({ currentUser }) => {
 };
 
 export async function getServerSideProps({ req }) {
-  let currentUser = {}; // Define data here
-
-  // console.log('host is ', req.headers);
-  // if (typeof window === 'undefined') {
-  //   // Server-side logic
-  //   const response = await axios.get(
-  //     'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-  //     {
-  //       headers: req.headers,
-  //     }
-  //   );
-  //   currentUser = response.data?.currentUser; // Set data from the server response
-  //   console.log('I am on Server Side context datta', currentUser);
-  // } else {
-  //   // Client-side logic
-  //   const response = await axios.get('/api/users/currentuser');
-  //   currentUser = response.data?.currentUser; // Set data from the client response
-  //   console.log('I am on Client Side context datta', currentUser);
-  // }
-
   // return { props: { currentUser } };
-  const client = await buildClient({ req }).get('/api/users/currentuser');
-  currentUser = client.data?.currentUser;
+  const client = await buildClient({ req });
+  const response = await client.get('/api/users/currentuser');
+  const currentUser = response.data?.currentUser;
   console.log('I am on Server Side context datta', currentUser);
   return { props: { currentUser } };
 }
