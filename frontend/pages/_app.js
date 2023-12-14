e9797bec2dd3528fc9daaace905702117ae8e946
unknown <sagarkhuteta26@gmail.com>
2023-12-14 12:39:00 -0500

    Issue with State Management and fetching in _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
- 
-export default function MyApp({ Component, pageProps }) {
+import buildClient from '../api/build-client'; 
+
+const AppComponent = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
+}
+
+AppComponent.getInitialProps = async (appContext) => {
+  console.log('I am on the app.js component', appContext.ctx);
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+  console.log('I am on the app.js component', data);
+  return data;
 }
---
+++ frontend/pages/auth/signin.js
@@ -0,0 +1,63 @@
+import React, { useReducer,useEffect } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case 'email':
+      return { ...state, email: action.payload };
+    case 'password':
+      return { ...state, password: action.payload };
+    default:
+      return state;
+  }
+};
+
+const signin = () => {
+  const [userState, userDispatch] = useReducer(userReducer, {
+    email: '',
+    password: '',
+  });
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signin',
+    method: 'post',
+    body: userState,
+    onSuccess: () => Router.push('/'),
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    console.log('Before updating Error', userState);
+    await doRequest();
+  };
+
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign In</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          className="form-control"
+          onChange={(event) => {
+            userDispatch({ type: 'email', payload: event.target.value });
+          }}
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          onChange={(event) => {
+            userDispatch({ type: 'password', payload: event.target.value });
+          }}
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign In</button>
+    </form>
+  );
+};
+
+export default signin;
